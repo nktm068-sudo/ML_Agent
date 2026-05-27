ec1cae8a5ffcb85d3fb1cd640887cefc35fa684e
Blue <nktm068@gmail.com>
2026-05-27 22:35:29 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const apiServerUrlSpaced = "h t t p s : / / a p i - i n f e r e n c e . h u g g i n g f a c e . c o / m o d e l s / Q w e n / Q w e n 2 . 5 - 7 2 B - I n s t r u c t";
 
 // Твой секретный токен, на сервере его никто не увидит
-const HF_KEY = "TOKEN"; 
+const HF_KEY = process.env.TOKEN;
 
 // Функция для очистки ссылки от пробелов
 function getCleanUrl(spacedUrl) {
@@ -21,7 +21,8 @@ app.post('/api/chat', async (req, res) => {
         const { query } = req.body;
         
         // Переключаемся на правильный URL для чат-моделей Qwen
-        const cleanUrl = "https://huggingface.co";
+        const cleanUrl = getCleanUrl("h t t p s : / / a p i - i n f e r e n c e . h u g g i n g f a c e . c o / m o d e l s / Q w e n / Q w e n 2 . 5 - 7 2 B - I n s t r u c t / v 1 / c h a t / c o m p l e t i o n s");
+
         
         const response = await fetch(cleanUrl, {
             method: "POST",
@@ -32,10 +33,10 @@ app.post('/api/chat', async (req, res) => {
             body: JSON.stringify({
                 model: "Qwen/Qwen2.5-72B-Instruct",
                 messages: [
-                    { role: "system", content: "Ты — полезный ИИ-помощник Миртекс. Отвечай кратко, понятно и на русском языке." },
+                    { role: "system", content: "Ты — Умный  и Гениальный помощник-программист Миртекс. Отвечай кратко, понятно, и когда видишь открытый апи ключ в коде пиши ТРЕВОГА!ОБНАРУЖЕН ОКТРЫТЫЙ АПИ КЛЮЧ! СРОчНО СПРЯЧЬ ЕГО! и на русском языке." },
                     { role: "user", content: query }
                 ],
-                max_tokens: 300
+                max_tokens: 1500
             })
         });
 
